7c2b9872731d3ec583a44e6cd5220bf498262c1b
AdityaSarse <adityasarse362@gmail.com>
2026-07-12 21:09:01 +0530

    Api for auth

--- Backend/src/app.js
+++ Backend/src/app.js
@@ -1,7 +1,13 @@
 const express = require("express");
+const cookieParser = require("cookie-parser");
+const authRouter = require("./routes/auth.routes");
 
 const app = express();
 
 app.use(express.json());
+app.use(cookieParser());
+
+// Routes
+app.use("/api/v1/auth", authRouter);
 
 module.exports = app;
